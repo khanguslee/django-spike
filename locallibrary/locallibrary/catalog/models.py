29b11477be29ab22fe132241ebdd0772b75340da
Kuan Hou Lee <angus.lee809@gmail.com>
2018-07-06 11:28:19 +1000

    Fixed __str__ in models.py

--- locallibrary/locallibrary/catalog/models.py
+++ locallibrary/locallibrary/catalog/models.py
@@ -7,7 +7,7 @@ class Genre(models.Model):
     """
     name = models.CharField(max_length=200, help_text="Enter a book genre(e.g. Science Fiction, French Poetry etc.)")
 
-    def __str_(self):
+    def __str__(self):
         """
         String for representing the Model object
         """
